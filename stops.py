ade82308feb0664e7481f9ad75171ab51e356ed2
Fahd <kirafahd232@gmail.com>
2025-01-06 22:41:38 +0100

    Add files via upload

---
+++ stops.py
@@ -0,0 +1,23 @@
+bus_stops = {
+    "L201B": [
+        "zerdal",
+        "lycee bouknadel",
+        "gare bouknadel",
+        "hopital bouknadel",
+        "com. bouknadel",
+        "entre bouknadel",
+        "res halima",
+        "doha",
+        "qantra",
+        "ex police bouknadel",
+        "ecole taha hesseine",
+        "rond point ihsan",
+        "avenue d",
+        "super",
+        "dar al hamra",
+        "tramway hay karima",
+        "houlyoud",
+        "souq assalihin",
+        "chateau hay salam",
+    ],
+}
